e42ddf59d65c21ba7742881c870eb983f9cb28e9
Xuzheng Lu <466532329@qq.com>
2019-01-01 12:09:45 +0800

    Fix a bug of file name

--- src/baseline_config.py
+++ src/baseline_config.py
@@ -47,7 +47,7 @@ __C.LR_DECAY_STEPS = 2000
 __C.LR_DECAY_RATE = 0.96
 
 # Epochs
-__C.EPOCHS = 10
+__C.EPOCHS = 50
 
 # Batch size
 __C.BATCH_SIZE = 2048
@@ -224,13 +224,13 @@ __C.SHOW_TRAINING_DETAILS = False
 # None: Do not test
 
 # Evaluate on single-object test set
-__C.TEST_SO_MODE = 'after_training'  # 'per_epoch'
+__C.TEST_SO_MODE = 'per_epoch'
 
 # Evaluate on multi-objects test set
-__C.TEST_MO_MODE = 'after_training'  # 'per_epoch'
+__C.TEST_MO_MODE = 'per_epoch'
 
 # Evaluate on Oracles test set after
-__C.TEST_ORACLE_MODE = None  # 'after_training'  # 'per_epoch'
+__C.TEST_ORACLE_MODE = None
 
 
 # ===========================================
